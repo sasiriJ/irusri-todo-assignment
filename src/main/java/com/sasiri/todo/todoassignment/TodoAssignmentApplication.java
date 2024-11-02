3aee39b61d907774154f5da84c3f19c29f2026ca
sasiri-jambugaswatta <jambugaswattasasiri@gmail.com>
2024-11-02 07:58:59 +0530

    initial commiot

---
+++ src/main/java/com/sasiri/todo/todoassignment/TodoAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.sasiri.todo.todoassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sasiri/todo/todoassignment/TodoAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sasiri.todo.todoassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
